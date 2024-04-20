7a37378d5a5dcd2f1154009a4adc3ef9aba89736
soragoto <soragoto@soragoto.io>
2024-04-20 14:44:55 +0800

    fix

--- Scripts/init.ps1
+++ Scripts/init.ps1
@@ -1,3 +1,4 @@
+# 添加中文
 $UserLanguageList = New-WinUserLanguageList -Language "zh-Hans-CN"
 
 # 删除默认输入法
@@ -10,6 +11,20 @@ $LanguageList = Get-WinUserLanguageList
 
 $LanguageList.Add($UserLanguageList[0])
 
+# 写入语言配置
 Set-WinUserLanguageList $LanguageList -Force
 
+# 导入输入法配置
 reg.exe import C:\Scripts\ime_conf.reg
+
+# 隐藏桌面回收站图标
+New-ItemProperty -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\NonEnum -Name "{645FF040-5081-101B-9F08-00AA002F954E}" -PropertyType Dword -Value 1
+
+# 自动隐藏任务栏
+$p='HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StuckRects3'
+$v=(Get-ItemProperty -Path $p).Settings
+$v[8]=3
+Set-ItemProperty -Path $p -Name Settings -Value $v
+
+# 重启explorer
+Stop-Process -f -ProcessName explorer
